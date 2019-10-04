0a1e7e2ee151e60454b1bf93a9d55adc93467e6a
Maica <gemaica.estrada@gmail.com>
2019-10-04 11:25:43 -0400

    first commit

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+        // Requires using System.Text.Encodings.Web;
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+           //ViewData["Mensahe"] = "Superb";
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
+
+        public string MyVeryOwnTrial()
+        {
+            return "Hahahaha. I can do it too! <3";
+        }
+    }
+}
